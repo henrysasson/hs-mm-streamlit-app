47b3f33ee20a6df13d29db1b7000c03ea35988ff
henrysasson <119268685+henrysasson@users.noreply.github.com>
2023-10-24 10:38:34 -0300

    Update hs_mm.py

--- hs_mm.py
+++ hs_mm.py
@@ -386,7 +386,7 @@ if selected == 'Macro Indicators':
             # Adicionar o símbolo de % ao eixo y
             fig_yc.update_yaxes(tickformat=".2f", ticksuffix="%")
 
-            fig_yc.update_layout( width=800,  # Largura do gráfico
+            fig_yc.update_layout( width=600,  # Largura do gráfico
         height=600  # Altura do gráfico
     )
 
