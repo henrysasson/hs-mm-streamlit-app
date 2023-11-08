b1af50bb04efaccd20e3bd368594480b1c7f087b
henrysasson <119268685+henrysasson@users.noreply.github.com>
2023-11-08 12:03:43 -0300

    Update hs_mm.py

--- hs_mm.py
+++ hs_mm.py
@@ -1573,8 +1573,7 @@ if selected == 'Positioning':
 def rank(series,n):
     # Calcula o percentil do último valor
     series = series[-n:]
-    x = series.iloc[-1]
-    percentile = stats.percentileofscore(series, x)
+    percentile = (np.searchsorted(np.sort(series), series.iloc[-1]) + 1) / len(series) * 100
     return round(percentile)
 
 col1, col2, col3 = st.columns(3)
