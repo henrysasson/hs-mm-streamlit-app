2a2e6a5ee04287d1ed6699e5240760df1dab28e5
henrysasson <119268685+henrysasson@users.noreply.github.com>
2023-11-28 14:22:56 -0300

    Update hs_mm.py

--- hs_mm.py
+++ hs_mm.py
@@ -2635,7 +2635,7 @@ height=600  # Altura do gráfico
         (all_assets_list))
 
     original_names = tickers_acoes + tickers_moedas + tickers_commodities + tickers_rf + tickers_crypto + tikckers_factors + tickers_sectors
-    trasformed_names = names_acoes + names_moedas + names_commodities + names_rf + names_crypto + name_factors + name_sectors
+    trasformed_names = names_acoes + names_moedas + names_commodities + names_rf + names_crypto + names_factors + names_sectors
     
     # Crie um dicionário de correspondência
     correspondencia = dict(zip(trasformed_names, original_names))
