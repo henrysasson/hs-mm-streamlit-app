13c19ec5f470794934c20a853fe096ba653c4960
henrysasson <119268685+henrysasson@users.noreply.github.com>
2024-02-16 11:13:01 -0300

    Update hs_mm.py

--- hs_mm.py
+++ hs_mm.py
@@ -158,7 +158,7 @@ if selected == 'Returns Heatmap':
         ))
         
         
-        fig.update_layout(title=classe, annotations=annotations, width=600,  # Largura do gráfico
+        fig.update_layout(autosize=True,title=classe, annotations=annotations, width=600,  # Largura do gráfico
     height=600  # Altura do gráfico
 )
         st.plotly_chart(fig)
